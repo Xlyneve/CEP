76d2f026e5dbba88f58ffd458f91034890acb05f
Xlyneve <xlyn02@icloud.com>
2026-03-07 12:29:11 +1300

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -19,6 +19,7 @@
     { name: 'male',   symbol: '♂️' },
     { name: 'email',  symbol: '🌐' },
     { name: 'phone',  symbol: '☎️' },
+    { name: 'yellow',  symbol: '🟡' },
   ];
 
   const IDS  = { btn: "emojiSearchIcon", modal: "emojiModal", grid: "emojiGrid" };
